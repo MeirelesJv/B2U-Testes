8a830fab3ccde334b1249ce8bddc8c803516b7ba
meirelesjv <meirelesjv@hotmail.com>
2026-03-16 11:43:09 -0300

    commit

--- Goflash/config.ts
+++
@@ -1,9 +0,0 @@
-// config.ts
-export const config = {
-  porta: "82",
-  usuario: "60",
-  senha: "123",
-  codProdutoBarra: "7890103788423",
-  cliente: "CLIENTE BALCAO - TORITAMA2",
-  cpf: "848.547.108-37",
-};
